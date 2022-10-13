5a4a786a810376c6ef5228094d00e042d36923a3
AlbinHall <albin-hall1@hotmail.com>
2022-10-13 19:42:00 +0000

    add more structure to javascript

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,11 +3,37 @@ let startButtonContainer = document.getElementById("start-btn")
 
 let answerButton = document.getElementById("holdingButtons")
 let questionHeader = document.getElementById("question")
+let extraButtonContainer = document.getElementById("extra-div")
+
 
 startButton.addEventListener("click", startGame)
 
+/**
+ * makes so that the sign up goes away and starts the quiz
+ */
 function startGame() {
     startButtonContainer.classList.add("hide")
     answerButton.classList.remove("hide")
     questionHeader.classList.remove("hide")
+    extraButtonContainer.classList.remove("hide")
 }
+
+function answers(){
+
+}
+
+function nextQuestion() {
+
+}
+
+let question1
+
+let question2
+
+let question3
+
+let question4
+
+let question5
+
+let question6
